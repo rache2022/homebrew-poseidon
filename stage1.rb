ad73699f1a186c47253ed6399736d7bb7deb40db
rache2022 <rache_bartmoss2022@proton.me>
2025-05-08 14:41:20 -0700

    Add files via upload

---
+++ stage1.rb
@@ -0,0 +1,14 @@
+class Stage1 < Formula
+  desc "Description of your script"
+  homepage "https://github.com/rache2022"
+  url "https://github.com/rache2022/homebrewed-poseidon/releases/download/v1.0.0/stage1.sh"
+  sha256 "06d64a1fe91cc886c0caee5474665ae0780a154c00628a8947ad7d83d82238db"
+
+  def install
+    bin.install "stage1.sh" => "stage1"
+  end
+
+  test do
+    system "#{bin}/stage1"
+  end
+end
