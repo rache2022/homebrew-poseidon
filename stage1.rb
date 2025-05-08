8b8f608305d82dd9453764fdabced074249e635d
rache2022 <rache_bartmoss2022@proton.me>
2025-05-08 15:33:11 -0700

    Update stage1.rb

--- stage1.rb
+++ stage1.rb
@@ -2,7 +2,7 @@ class Stage1 < Formula
   desc "Description of your script"
   homepage "https://github.com/rache2022"
   url "https://github.com/rache2022/homebrewed-poseidon/releases/download/v1.0.0/stage1.sh"
-  sha256 "06d64a1fe91cc886c0caee5474665ae0780a154c00628a8947ad7d83d82238db"
+  sha256 "d548702736fced88896492c82bcde0f9bd10e69d9366a09a19747101b4b4fc2b"
 
   def install
     bin.install "stage1.sh" => "stage1"
